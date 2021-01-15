e23e1bf12d8a5913ec35a2efac96521f1107f926
nathan joseph <josephnathan10@gmail.com>
2021-01-15 14:35:02 -0900

    medium mode finished

---
+++ index.js
@@ -0,0 +1,11 @@
+function App(props) {
+  return (
+    <div className="App">
+      <h1>Welcome to the sports game starter.</h1>
+      This file represents the code after completing the setup step in the lab
+      instructions.
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
